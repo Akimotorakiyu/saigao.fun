68eba0e3d54660242917e29b4c744436a8312042
autumnsilverleaves@gmail.com <autumnsilverleaves@gmail.com>
2018-07-27 22:12:46 +0800

    新版搭建完毕
    基于VuePress默认主题的新版搭建完毕

---
+++ new/docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports={
+    // 默认标题，会被地区标题覆盖
+    title:"Silver湫澲",
+    // 默认描述，会被地区描述覆盖
+    description:"Z(t+Δ)=f[Z(t),t+Δ]",
+    base:"/",
+    dest:"../build",
+    port:"8080",
+    serviceWorker:"true",
+    locales:{
+        "/":{
+            lang:"zh-CN",
+        },
+    },
+    themeConfig:{
+        sidebarDepth:2,
+        sidebar: 'auto',
+        lastUpdated: 'Last Updated',
+        nav:[
+            {text:"主页",link:"/"},
+            // {text:"笔记",link:"/note/"},
+            // {text:"文集",link:"/wenji/"},
+            // {text:"关于",link:"/about"},
+        ],
+        sidebar:{
+            // 除此之外还可以自定侧边栏
+            '/':[{
+                title: '摘要',
+                collapsable: false,
+                children: [
+                  ['/','主页'],
+                  ['/about','关于'],
+                ]
+              },]
+        }
+    }
+}
---
+++ new/docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /hero.png
+actionText: P(X=n)=1-p^n n∈N+ →
+actionLink: /note/
+features:
+- title: Silver·西尔弗
+  details: 西尔弗银匠，银色品质。
+- title: 湫澲·水火禾业
+  details: 精耕细作，丰喜湫澲。
+- title: Silver·湫澲
+  details: 业精于勤，行成于思。
+footer: 我们所过的每个平凡的日常，也许就是连续发生的奇迹。 Copyright © 2018-Present Silver·湫澲
+---
+
+<!-- 注意，每个冒号后必须有个空格 -->
